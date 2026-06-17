4acee359d4196b2a795a64210290152e21d2c6b9
Trần Văn Mỹ <tvmptit0511@gmail.com>
2026-06-17 10:44:12 +0700

    Add bai5/data/__init__.py

---
+++ bai5/data/__init__.py
@@ -0,0 +1,7 @@
+"""
+data/__init__.py
+------------------
+Biến thư mục data/ thành một package Python, đảm bảo import
+"from data.players import player_records" hoạt động ổn định khi chạy
+chương trình bằng "python main.py" từ thư mục gốc project.
+"""
